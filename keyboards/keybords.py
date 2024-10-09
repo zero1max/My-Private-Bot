4fa8edffc753d940f7f157a44d5b4305297578cf
Nigmatjonov Muhammadjon <148071379+zero1max@users.noreply.github.com>
2024-10-09 20:39:57 +0500

    Update Commit

--- keyboards/keybords.py
+++ keyboards/keybords.py
@@ -2,8 +2,8 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 send_msg = ReplyKeyboardMarkup(
         resize_keyboard=True,
-        one_time_keyboard=True,
         keyboard=[
-            [KeyboardButton(text="Savol yuborish✍🏻")]
+            [KeyboardButton(text="Savol yuborish✍🏻")],
+            [KeyboardButton(text='Anonim savol yuborish🤫✍️')]
         ]
 )
